c26efd52677337629c6b2719401df693606f0aba
Marie Katrine Ekeberg <spacemarky@gmail.com>
2018-10-21 13:06:12 +0200

    Improved mockmvc test. Now all features work in testing. Testing as a dependency still needed.

--- src/test/kotlin/MockMvcSpec.kt
+++ src/test/kotlin/MockMvcSpec.kt
@@ -8,25 +8,31 @@ import net.themkat.spek.spring.utils.testing.data.GameRepository
 import net.themkat.spek.spring.utils.testing.data.GameRestController
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
-import java.util.*
-
+import java.util.Optional
 
 object MockMvcSpec : Spek({
     val gameRepository = mockk<GameRepository>()
     val gameRestController = GameRestController(gameRepository)
 
     mockMvc(gameRestController) {
-        val mockMvc = mockMvc()
+        val mockMvc = standaloneSetup()
 
         describe("fetching some data using mockmvc from the controller") {
             it("should return game given id") {
                 val gameId = 1
+                val gameName = "Turok 2: Seeds of Evil"
+                val gameReleaseYear = 1998
+                val gameDeveloper = "Acclaim Entertainment"
+
                 every { gameRepository.findById(gameId) }
-                        .returns(Optional.of(Game(gameId, "Turok 2: Seeds of Evil", 1998, "Acclaim Entertainment")))
+                        .returns(Optional.of(Game(gameId, gameName, gameReleaseYear, gameDeveloper)))
 
                 mockMvc.perform(get("/game/$gameId"))
                         .andExpect(status().isOk())
                         .andExpect(jsonPathEquals("$.id", "$gameId"))
+                        .andExpect(jsonPathEquals("$.name", "$gameName"))
+                        .andExpect(jsonPathEquals("$.releaseYear", "$gameReleaseYear"))
+                        .andExpect(jsonPathEquals("$.developer", "$gameDeveloper"))
             }
         }
     }
